0fe1bfa4178a1087e45c0516965275a4f561bcf8
Arvedson <tomasarvedson@gmail.com>
2025-03-22 13:15:24 -0600

    firebase key fix

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -6,7 +6,8 @@ if (!admin.apps.length) {
         // Initialize Firebase Admin SDK with environment variables
         const firebaseConfig = {
             projectId: process.env.FIREBASE_PROJECT_ID,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // Corregir saltos de línea
+            privateKey: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
+
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         };
 
